7e2e6988e29e424e0b284083c26408d4c944eed2
shruti <shrutibansal1802@gmail.com>
2020-08-31 22:31:44 +0530

    nav color changing

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const bgImage = document.getElementsByClassName('bgImg');
+let imageEnd = bgImage[0].clientHeight;
+
+$(window).resize(()=>{imageEnd= bgImage[0].clientHeight})
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop()>imageEnd)
+   })

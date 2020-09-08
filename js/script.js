bd7cd9304dac3cfe07c714f918bb1ad03a799243
shruti <shrutibansal1802@gmail.com>
2020-09-08 20:23:25 +0530

    navcolor change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const bgImage = document.getElementsByClassName('bgImg');
-let imageEnd = bgImage[0].clientHeight;
+const bgImage = document.querySelector('.carousel-inner');
+let imageEnd = bgImage.clientHeight;
 
-$(window).resize(()=>{imageEnd= bgImage[0].clientHeight})
+$(window).resize(()=>{imageEnd= bgImage.clientHeight})
 
 $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop()>imageEnd)
    })
-

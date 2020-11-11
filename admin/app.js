2c1d2a70f213cc5e2675bdf59e516be1409b4dbc
Shubham Gautam <sg2199203@gmail.com>
2020-11-11 14:17:29 +0530

    Added Encryption for Username

--- admin/app.js
+++ admin/app.js
@@ -4,10 +4,17 @@ loginForm.addEventListener('submit', login);
 function login(e){
     e.preventDefault(); //to stop actions
     var userN = document.getElementById('userN').value;
+    var encryptedUser = CryptoJS.AES.encrypt(userN, "Secret Passphrase");
+    var decryptedUser = CryptoJS.AES.decrypt(encryptedUser, "Secret Passphrase");
+
     var pwd = document.getElementById('pwd').value;
-    var encryptedPwd = CryptoJS.AES.encrypt(pwd, "Secret Passphrase");
-    var decryptedPwd = CryptoJS.AES.decrypt(encryptedPwd, "Secret Passphrase");
-    if (decryptedPwd == '746563686b6e696768747341646d696e'){
-        window.location = "https://github.com/"
+    var encryptedPwd = CryptoJS.AES.encrypt(pwd, "Secret Passphrase"); //encrypting password
+    var decryptedPwd = CryptoJS.AES.decrypt(encryptedPwd, "Secret Passphrase"); //descrepting password
+    
+    if (decryptedUser == '746563686b6e696768747341646d696e' && decryptedPwd == '746563686b6e696768747341646d696e'){ //remains same always
+        alert('success');
+    }
+    else{
+        alert('Sorry! Can not login');
     }
 }

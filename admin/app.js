7ddbfa7c38ffffc596eb2642947c0daa2d02a05d
Shubham Gautam <sg2199203@gmail.com>
2020-11-11 22:50:02 +0530

    Added Security

--- admin/app.js
+++ admin/app.js
@@ -1,3 +1,17 @@
+//checks if the details present are true or not
+firebase.auth().onAuthStateChanged((user) => {
+    //if user exists - provide entry
+    if (user) {
+
+        var user = firebase.auth().currentUser;
+        if (user != null) {
+            // window.location = add[1].toString(CryptoJS.enc.Utf8);
+            window.location = "../index.html";
+        }
+
+    }
+});
+
 //global - For this line refer forUs.docx (Password with Shubham)
 eval(function (p, a, c, k, e, d) { e = function (c) { return c }; if (!''.replace(/^/, String)) { while (c--) { d[c] = k[c] || c } k = [function (e) { return d[e] }]; e = function () { return '\\w+' }; c = 1 }; while (c--) { if (k[c]) { p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]) } } return p }('1 17=[0("../16/4.5"),0("../15/4.5")];14 0(6){1 8=3.7.13(6,"9 10");1 2=3.7.11(8,"9 10");12 2}', 10, 18, 'performEncr|var|decryptedStr|CryptoJS|index|html|addStr|AES|encryptedStr|Secret|Passphrase|decrypt|return|encrypt|function|log|admin|add'.split('|'), 0, {}))
 
@@ -11,14 +25,6 @@ function login(e){
     e.preventDefault(); //to stop actions
     var userN = document.getElementById('userN').value;
     var pwd = document.getElementById('pwd').value;
-
-    //checks if the details present are true or not
-    firebase.auth().onAuthStateChanged((user) => {
-        //if user exists - provide entry
-        if (user) {
-            window.location = add[1].toString(CryptoJS.enc.Utf8);
-        }
-    });
 
     firebase.auth().signInWithEmailAndPassword(userN, pwd).catch(function (error) {
         // Handle Errors here.

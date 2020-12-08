3cb96124c1472bfbb4fd9da48f45d87aa5d4c507
Shubham Gautam <sg2199203@gmail.com>
2020-12-08 09:00:45 +0530

    Added Firebase Modal

--- js/script.js
+++ js/script.js
@@ -36,7 +36,40 @@ function firebaseCallPull(){
               <div class="event-fee">${newEvent.eventFee}</div>
             </div>
           </div>
-        </div>`;
+        </div>
+        <div class="modal fade" id="regForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                       <h5 class="modal-title" id="exampleModalLongTitle">${newEvent.eventName}</h5>
+                       <p class="modal-description">${newEvent.eventDesc}</p></div>
+
+                       <div class="modal-body">
+                            <form class="regform" action="get" name="registrationForm">
+                                <input type="text" name="name" id="name" placeholder="name">
+                                <input type="email" name="email" id="email" placeholder="email">
+                                <input type="number" name="phone-number" id="phone-number" placeholder="phone-number">
+                                <label for="year">Select your year:</label>
+                                <select name="year" id="year" form="registrationForm">
+                                    <option value="1">1st year</option>
+                                    <option value="2">2nd year</option>
+                                    <option value="3">3rd year</option>
+                                    <option value="4">4th year</option>
+                                </select>
+                                <label for="comments">Any comments</label>
+                                <textarea name="comments" id="comments" cols="10" rows="5"></textarea>
+                            </form>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Register</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>`
+        ;
         var regBtn = document.querySelector('.register');
         regBtn.setAttribute('id', `${newEvent.eventName}`);
 

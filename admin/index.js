92b1ef5d727114c25bd48b6ebacd945a8cb43184
Shubham Gautam <sg2199203@gmail.com>
2020-12-29 12:08:42 +0530

    Added form

---
+++ admin/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', crawlAnim)
+
+async function crawlAnim(){
+    var welTxt = document.querySelector('.welcome-txt')
+    welTxt.style.animation = 'crwalUp 1.5s 0s ease'
+
+}

1d27e7c38efdb0084e0d41890de75eadead9e623
Shubham Gautam <sg2199203@gmail.com>
2020-12-29 12:16:43 +0530

    Rewriting the script

--- admin/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', crawlAnim)
-
-async function crawlAnim(){
-    var welTxt = document.querySelector('.welcome-txt')
-    welTxt.style.animation = 'crwalUp 1.5s 0s ease'
-
-}

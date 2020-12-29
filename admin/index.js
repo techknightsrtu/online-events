a84e8c9a5342bb6d421c834ec4e30396e11f5e8e
Shubham Gautam <sg2199203@gmail.com>
2020-12-29 20:15:09 +0530

    Completed

--- admin/index.js
+++ admin/index.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', crawlAnim)
 
+//for animation at the start
 function crawlAnim() {
     var welTxt = document.querySelector('.welcome-txt')
     welTxt.style.animation = `crawlUp 1.6s 0s ease-in-out`
@@ -10,48 +11,72 @@ function crawlAnim() {
     }, 1500);
 }
 
+//main-form
 var form = document.querySelector('form')
 form.addEventListener('submit', runAuthentication)
 
-function runAuthentication(){
+//authentication 
+function runAuthentication(e){
+    e.preventDefault();
+    //password prompt
     var keyCode = document.querySelector('.key-code')
     keyCode.classList.add('show-me')
-
+    
+    //password area
     var validationForm = document.querySelector('.validation-form')
     validationForm.addEventListener('submit', runCipher)
+    
+    //to close the promt after event has been submitted
+    keyCode.classList.add('show-me')
+    
+    //reset validation form
+    validationForm.reset()
 }
 
+//running cipher
 function runCipher(e){
     e.preventDefault();
     var pwdStr = document.querySelector('#pwd-str').value
     var keyNo = document.querySelector('#key-pwd').value
     
     var decryptedPwd = ''
-    let decryptedLtr = ''
+    let decryptedLtr
+    
+    //checking pwd by invoking
     for (let pwd = 0; pwd < pwdStr.length; pwd++) {
-        decryptedLtr = letterDecryption(pwd[i], keyNo)
-
+        decryptedLtr = letterDecryption(pwdStr[pwd], keyNo)
         decryptedPwd += decryptedLtr
     }
-    if (decryptedPwd === 'cd') {
+    
+    if (decryptedPwd === 'zkinqtomnzyajsot') {
         fetchValues()
+    }
+    else{
+        alert('Not Correct')
     }
 }
 
 function letterDecryption(letter, key){
-
+    //fetching ascii value of letter
     var ltrAscii = letter.charCodeAt(0);
+    
+    //ascii value of 'a'
     var aAscii = 97
     var alphaSize = 26
-    var trueValue = aAscii + (((ltrAscii - aAscii) + key) % alphaSize)
-    var decryptedLtr = String.fromCharCode(trueValue)
 
-    return decryptedLtr
+    //algorithm
+    var trueValue = aAscii + (((ltrAscii - aAscii) + Number(key)) % alphaSize)
+
+    //decrypted letter
+    var decryLtr = String.fromCharCode(trueValue)
+    
+    return decryLtr
 }
 
-var refVariable = firebase.database().ref().child('Events/');
-function fetchValues(evt){
-    evt.preventDefault()
+//firebase refernce varaible
+var eventReference = firebase.database().ref().child('Events/');
+function fetchValues(){
+    //getting values
     var eventCode = document.querySelector('#eventCode').value;
     var eventDate = document.querySelector('#eventDT').value;
     var eventName = document.querySelector('#eventName').value;
@@ -60,12 +85,13 @@ function fetchValues(evt){
     var eventFee = document.querySelector('#eventFee').value;
     var eventDesc = document.querySelector('#eventDesc').value;
     
+    //saving to firebase
     saveEvent(eventCode, eventDate, eventName, eventType, eventVenue, eventFee, eventDesc);
 }
 
 function saveEvent(evtCode, evtDt, evtNm, evtType, evtVenue, evtFee, evtDesc) {
-    var newRefVariable = refVariable.push();
-    newRefVariable.set(
+    var newEvent = eventReference.push();
+    newEvent.set(
     {
         eventCode: evtCode,
         eventDate: evtDt,
@@ -76,6 +102,7 @@ function saveEvent(evtCode, evtDt, evtNm, evtType, evtVenue, evtFee, evtDesc) {
         eventVenue: evtVenue
     });
 
+    //main form reset
     form.reset();
 }
 

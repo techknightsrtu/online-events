ca6b46f6bebb2a49f5b762b7cac148f4f702b2c1
Shubham Gautam <sg2199203@gmail.com>
2020-12-29 19:29:24 +0530

    Implemented cipher algo

--- admin/index.js
+++ admin/index.js
@@ -14,7 +14,39 @@ var form = document.querySelector('form')
 form.addEventListener('submit', runAuthentication)
 
 function runAuthentication(){
+    var keyCode = document.querySelector('.key-code')
+    keyCode.classList.add('show-me')
+
+    var validationForm = document.querySelector('.validation-form')
+    validationForm.addEventListener('submit', runCipher)
+}
+
+function runCipher(e){
+    e.preventDefault();
+    var pwdStr = document.querySelector('#pwd-str').value
+    var keyNo = document.querySelector('#key-pwd').value
     
+    var decryptedPwd = ''
+    let decryptedLtr = ''
+    for (let pwd = 0; pwd < pwdStr.length; pwd++) {
+        decryptedLtr = letterDecryption(pwd[i], keyNo)
+
+        decryptedPwd += decryptedLtr
+    }
+    if (decryptedPwd === 'cd') {
+        fetchValues()
+    }
+}
+
+function letterDecryption(letter, key){
+
+    var ltrAscii = letter.charCodeAt(0);
+    var aAscii = 97
+    var alphaSize = 26
+    var trueValue = aAscii + (((ltrAscii - aAscii) + key) % alphaSize)
+    var decryptedLtr = String.fromCharCode(trueValue)
+
+    return decryptedLtr
 }
 
 var refVariable = firebase.database().ref().child('Events/');
